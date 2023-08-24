f597c9034aa9a7aefb0860e291c651330a280d11
dhiakhalfaoui <dhiakhafaloui737@gmail.com>
2023-08-24 15:48:42 +0100

    questions

---
+++ questions.js
@@ -0,0 +1,64 @@
+let questions = [
+    {
+        Numb: 1,
+        question: "what does HTML stand for ",
+        answer: "C. Hyper Text markup language",
+        options :[
+            "A. Hyper Type Multi language",
+            "B. Hyper Text Multiple  language",
+            "C. Hyper Text Markup language",
+            "D. Home Text Multi language"
+
+        ]
+    },
+    {
+        Numb: 2,
+        question: "what does CSS stand for ",
+        answer: "A. Cascading Style Sheet",
+        options :[
+            "A. Cascading Style Sheet",
+            "B. Cute style Sheet",
+            "C. Computer style sheet",
+            "D. Codehal Style Sheet",
+
+        ]
+    },
+    {
+        Numb: 3,
+        question: "what is meaw",
+        answer: "A. meaw is haha",
+        options :[
+            "A. meaw is hahah",
+            "B. MIMIMI",
+            "C. MOMOMOM",
+            "D. NMMNMNMNM"
+
+        ]
+    },
+    {
+        Numb: 4,
+        question: "WHAT IS KOKO",
+        answer: "B. KOKO IS DODO",
+        options :[
+            "A. SDFSBVSV",
+            "B. KOKO IS DODO",
+            "C. SFVFSFSVS",
+            "D. SGVSFBBSBV"
+
+        ]
+    },
+    {
+        Numb: 5,
+        question: " WHAT IS NONO",
+        answer: " C.NONO IS HEAR",
+        options :[
+            "A. ADSFAGFSG",
+            "B. SDDSGSDSDG",
+            "C. NON IS HEAR",
+            "D. SDGDSGSDF"
+
+        ]
+    },
+];
+
+
